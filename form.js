1cc9b5f3c32643968f163923fb6b651d6e07c9d0
Shel Valdés <design@iMac-de-Shel.localdomain>
2021-08-31 18:35:56 -0500

    First commit

---
+++ form.js
@@ -0,0 +1,17 @@
+const labels = [
+    'First Name',
+    'Last Name',
+    'Address'
+]
+
+const placeholders = [
+    'John',
+    'Doe'
+    'Your Address'
+]
+
+const makeForm = () =>{
+    alert("miau");
+}
+
+makeForm();
