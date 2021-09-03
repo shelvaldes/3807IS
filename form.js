abc35d1299d51a605036383c449118bd24375c93
Shel Valdés <design@iMac-de-Shel.local>
2021-09-03 14:05:30 -0500

    Error message and general CSS for IS

--- form.js
+++ form.js
@@ -4,26 +4,34 @@ function validateField (event){
     let fieldValue = event.value
     let image = event.parentNode.children[2].children[0]
     let formField = event.parentNode.children[1]
-    console.log(formField)
+    let errorBox = event.parentNode.children[3]
+    //console.log(errorBox)
     if (fieldValue === ''){
         image.classList.remove("formfield__checkmark--unhide");
-        formField.classList.remove("formfield__box--validated")
+        formField.classList.remove("formfield__box--validated");
+        formField.classList.add("formfield__box--error");
+        errorBox.classList.add("form__error--unhide", "form__error--unhide::before", "miau");
+        errorBox.classList.remove("form__error--hidden")
     }else{
         image.classList.add("formfield__checkmark--unhide");
-        formField.classList.add("formfield__box--validated")
+        formField.classList.add("formfield__box--validated");
+        formField.classList.remove("formfield__box--error");
+        errorBox.classList.remove("form__error--unhide", "form__error--unhide::before");
+        errorBox.classList.add("form__error--hidden")
     }
 }
 
 const makeForm = (label, id, placeholder) =>{
     const formFieldTemplate =
         `<div class="input-container">
-        <label for="input${label}">${label}</label>
-        <input id="input${id}" onfocusout="validateField(this)" type="text" placeholder="${placeholder}">
+            <label for="input${label}">${label}</label>
+            <input id="input${id}" onblur="validateField(this)" type="text" placeholder="${placeholder}">
         
-        <div class="checkmark-container">
-        <img class="checkmark" id="img${id}" src="img/check-mark.svg" alt="checkmark">
-        </div>
-    </div>`
+            <div class="checkmark-container">
+                <img class="checkmark" id="img${id}" src="img/check-mark.svg" alt="checkmark">
+            </div>
+            <div class="form__error--hidden">This is the error message</div>
+        </div>`
     
    //return formFieldTemplate
    emptyForm.innerHTML = emptyForm.innerHTML + formFieldTemplate
